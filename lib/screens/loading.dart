413b8ab38ecfc4c258cd70695cac9eb75099b099
mike.bskim <mike.bskim@gmail.com>
2022-04-27 16:30:57 +0200

    json

--- lib/screens/loading.dart
+++ lib/screens/loading.dart
@@ -1,5 +1,9 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:http/http.dart' as http;
 
 class Loading extends StatefulWidget {
   const Loading({Key? key}) : super(key: key);
@@ -9,14 +13,35 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getLocation();
+    fetchData();
+  }
 
-  void getLocation() async{
+  void getLocation() async {
+    try {
+      LocationPermission permission = await Geolocator.requestPermission();
+      Position position = await Geolocator.getCurrentPosition(
+          desiredAccuracy: LocationAccuracy.high);
+      debugPrint(position.toString());
+    } catch (e) {
+      debugPrint('internet connection Error');
+    }
+  }
 
-    LocationPermission permission = await Geolocator.requestPermission();
-    Position position = await Geolocator.
-    getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
-    debugPrint(position.toString());
-    debugPrint(position.latitude.toString() + ' / ' + position.longitude.toString());
+  void fetchData() async {
+    http.Response response = await http.get(Uri.parse(
+        'https://samples.openweathermap.org/data/2.5/weather?q=London&appid=b1b15e88fa797225412429c1c50c122a1'));
+
+    if (response.statusCode == 200) {
+      String jsonData = response.body;
+      var myJson = jsonDecode(jsonData)['weather'][0]['description'];
+      debugPrint(myJson);
+    } else {}
+    debugPrint(response.body.toString());
   }
 
   @override
@@ -25,14 +50,12 @@ class _LoadingState extends State<Loading> {
       body: Center(
         child: ElevatedButton(
           onPressed: () {
-          debugPrint('ElevatedButton clicked~~');
-            getLocation();
+            debugPrint('ElevatedButton clicked~~');
+            fetchData();
           },
           child: const Text(
             'Get my location',
-            style: TextStyle(
-                color: Colors.white
-            ),
+            style: TextStyle(color: Colors.white),
           ),
         ),
       ),
