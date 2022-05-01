946a65f8f81829ee8f16f073ecb5fe76da6ab932
mike.bskim <mike.bskim@gmail.com>
2022-05-01 14:36:36 +0200

    indicator

--- lib/screens/loading.dart
+++ lib/screens/loading.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:weather/data/my_location.dart';
 import 'package:weather/data/network.dart';
 // import 'package:weather/model/air_model.dart';
@@ -73,16 +74,12 @@ class _LoadingState extends State<Loading> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
+      backgroundColor: Colors.amber,
       body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            debugPrint('ElevatedButton clicked~~');
-          },
-          child: const Text(
-            'Get my location',
-            style: TextStyle(color: Colors.white),
-          ),
+        child: SpinKitDoubleBounce(
+          color: Colors.white,
+          size: 80.0,
         ),
       ),
     );
